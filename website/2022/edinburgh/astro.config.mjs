df5a057382d8f86d786616aa1a2992264159575a
David Flanagan <david@rawkode.dev>
2023-01-19 21:20:56 +0000

    feat: prepapre for multiple years and locations

--- website/2022/edinburgh/astro.config.mjs
+++ website/2022/edinburgh/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+	base: process.env.ASTRO_BASE_PATH || "",
   vite: {
     ssr: {
       external: ["svgo"],
     },
   },
-  integrations: [preact()],
+  integrations: [],
 });
