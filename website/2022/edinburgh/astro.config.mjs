346908667b0c25552ea205675251d76f475acc2b
David Flanagan <david@rawkode.dev>
2023-01-19 22:44:14 +0000

    chore: base url for edinburgh 2022

--- website/2022/edinburgh/astro.config.mjs
+++ website/2022/edinburgh/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	base: process.env.ASTRO_BASE_PATH || "",
+	base: "/2022/edinburgh/",
   vite: {
     ssr: {
       external: ["svgo"],
